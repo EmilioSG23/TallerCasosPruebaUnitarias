4297120d3cff0a11f70270c62923db765d6e4d62
EmilioSG23 <93797627+EmilioSG23@users.noreply.github.com>
2022-08-24 10:54:07 -0500

    Tests

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -12,8 +12,50 @@ class EmployeeTest {
 	@Test
 	//Prueba constructores
 	public void testEmployee() {
-		Employee e=new Employee(386,"USD",15,EmployeeType.Worker);
+		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
 		assertNotNull(e);
 	}
 	
+	public void testWorkerSalary() {
+		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		//Caso impar
+		//assertEquals(444.33,e.cs());
+		//Caso par
+		assertEquals(380,e.cs());
+	}
+	public void testSupervisorSalary() {
+		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		//Caso impar
+		//assertEquals(444.33,e.cs());
+		//Caso par
+		assertEquals(380,e.cs());
+	}
+	public void testManagerSalary() {
+		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		//Caso impar
+		//assertEquals(444.33,e.cs());
+		//Caso par
+		assertEquals(380,e.cs());
+	}
+	public void testCurrencySalaryNotUSD() {
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+		assertEquals(95,e.cs());
+	}
+	
+	public void testWorkerBonus() {
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+		
+	}
+	public void testSupervisorBonus() {
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+	}
+	
+	public void testManagerBonus() {
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+		
+	}
+	public void testCurrencyBonusNotUSD() {
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+		
+	}
 }
