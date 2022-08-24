434c5085406aceefcfb594ceeafdeff3dcfc3476
EmilioSG23 <93797627+EmilioSG23@users.noreply.github.com>
2022-08-24 11:00:25 -0500

    Mas tests

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -16,22 +16,23 @@ class EmployeeTest {
 		assertNotNull(e);
 	}
 	
+	
 	public void testWorkerSalary() {
-		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		Employee e=new Employee(100,"USD",15,EmployeeType.Worker);
 		//Caso impar
 		//assertEquals(444.33,e.cs());
 		//Caso par
-		assertEquals(380,e.cs());
+		assertEquals(100,e.cs());
 	}
 	public void testSupervisorSalary() {
-		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		Employee e=new Employee(100,"USD",15,EmployeeType.Supervisor);
 		//Caso impar
 		//assertEquals(444.33,e.cs());
 		//Caso par
 		assertEquals(380,e.cs());
 	}
 	public void testManagerSalary() {
-		Employee e=new Employee(380,"USD",15,EmployeeType.Worker);
+		Employee e=new Employee(100,"USD",15,EmployeeType.Manager);
 		//Caso impar
 		//assertEquals(444.33,e.cs());
 		//Caso par
@@ -42,20 +43,24 @@ class EmployeeTest {
 		assertEquals(95,e.cs());
 	}
 	
+	@Test
 	public void testWorkerBonus() {
-		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
-		
+		Employee e=new Employee(100,"USD",15,EmployeeType.Worker);
+		assertEquals(386,e.CalculateYearBonus());
 	}
+	@Test
 	public void testSupervisorBonus() {
-		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
+		Employee e=new Employee(100,"USD",15,EmployeeType.Supervisor);
+		assertEquals(293,e.CalculateYearBonus());
 	}
-	
+	@Test
 	public void testManagerBonus() {
-		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
-		
+		Employee e=new Employee(100,"USD",15,EmployeeType.Manager);
+		assertEquals(486,e.CalculateYearBonus());
 	}
+	@Test
 	public void testCurrencyBonusNotUSD() {
-		Employee e=new Employee(100,"Euro",15,EmployeeType.Worker);
-		
+		Employee e=new Employee(100,"Euro",15,EmployeeType.Supervisor);
+		assertEquals(288,e.CalculateYearBonus());
 	}
 }
